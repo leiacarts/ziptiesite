23bb612c61187b4b0a0930b023e14c6a87397cff
Leia C <leiacarts@users.noreply.github.com>
2020-03-15 03:39:23 -0400

    hide/show sections

--- app.js
+++ app.js
@@ -1,22 +1,28 @@
+//select elements
 var sections = document.querySelectorAll(".section")
 
+//give each element the click event
 sections.forEach(function(section) {
+  //callback function
   section.addEventListener("click", expandSection);
 })
 
+//what happens on click
 function expandSection(event) {
+
 let section = (event.target.classList.contains("section")) ? event.target : event.target.parentNode;
-  
+
 let images = (event.target.classList.contains("images")) ? event.target : event.target.parentNode;
 
-
 sections.forEach(function(section) {
-  section.classList.remove("open")
-  images.classList.remove("show");
-  images.classList.add("hide");
+  //callback function
+  section.classList.remove("open");
+  section.classList.add("hide");
+  section.classList.remove("show");
 })
 
+// give class opener
 section.classList.add("open");
-images.classList.remove("hide");
-images.classList.add("show");
-}
+section.classList.remove("hide");
+section.classList.add("show");
+};
