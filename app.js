4331fb060ec99f13a95abb421a2d537fbdd5c151
Leia C <leiacarts@users.noreply.github.com>
2020-03-15 03:18:50 -0400

    Update app.js

--- app.js
+++ app.js
@@ -6,10 +6,17 @@ sections.forEach(function(section) {
 
 function expandSection(event) {
 let section = (event.target.classList.contains("section")) ? event.target : event.target.parentNode;
+  
+let images = (event.target.classList.contains("images")) ? event.target : event.target.parentNode;
+
 
 sections.forEach(function(section) {
   section.classList.remove("open")
+  images.classList.remove("show");
+  images.classList.add("hide");
 })
 
 section.classList.add("open");
+images.classList.remove("hide");
+images.classList.add("show");
 }
